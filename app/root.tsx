22f30f4b17edf478a052a38f23f8b0c818e932cf
Josh Goldberg <git@joshuakgoldberg.com>
2023-04-15 15:38:31 -0400

    fix: refer to correct logo.png for metadata

--- app/root.tsx
+++ app/root.tsx
@@ -12,28 +12,31 @@ import styles from "./root.css";
 const description =
 	"A local community for talking about the things that excite us about Javascript development and the web.";
 
+const metaTags = [
+	{ charSet: "utf-8" },
+	{ content: "https://phillyjs.com", property: "og:url" },
+	{ content: "https://phillyjs.com", property: "twitter:url" },
+	{ content: "https://phillyjs.com/logo.png", property: "og:image" },
+	{ content: "https://phillyjs.com/logo.png", property: "twitter:image" },
+	{ content: "Philadelphia JavaScript Club", property: "og:title" },
+	{ content: "Philadelphia JavaScript Club", property: "twitter:title" },
+	{ content: "Philly JS Club", name: "title" },
+	{ content: "summary_large_image", property: "twitter:card" },
+	{ content: "website", property: "og:type" },
+	{ content: "width=device-width,initial-scale=1", name: "viewport" },
+	{ content: description, name: "description" },
+	{ content: description, property: "twitter:description" },
+];
+
 export default function App() {
 	return (
 		<html lang="en">
 			<head>
-				<meta charSet="utf-8" />
-				<meta content={description} name="description" />
-				<meta content="Philly JS Club" name="title" />
-				<meta content="width=device-width,initial-scale=1" name="viewport" />
-				<meta content="Philadelphia JavaScript Club" property="og:title" />
-				<meta content="website" property="og:type" />
-				<meta content="https://phillyjs.com" property="og:url" />
-				<meta content="summary_large_image" property="twitter:card" />
-				<meta content={description} property="twitter:description" />
-				<meta content="Philadelphia JavaScript Club" property="twitter:title" />
-				<meta content="https://phillyjs.com" property="twitter:url" />
-				<meta
-					content="https://phillyjs.com/logo.svg"
-					property="twitter:image"
-				/>
+				{metaTags.map((meta) => (
+					<meta key={JSON.stringify(meta)} {...meta} />
+				))}
 				<link href="https://fonts.googleapis.com" rel="preconnect" />
 				<link href={styles} rel="stylesheet" />
-
 				<Meta />
 				<Links />
 			</head>
