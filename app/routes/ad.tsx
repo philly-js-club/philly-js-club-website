4bd9cdb31afe4f550805088492bee444f879a141
Ron Braha <45559220+RNR1@users.noreply.github.com>
2023-04-30 13:37:30 -0400

    fix: add page title to ad page, resolves #17 (#18)
    * fix: add page title to ad page
    
    * fix: remove unnecessary generic
    
    * refactor(ad): extract month and year logic to utils

--- app/routes/ad.tsx
+++ app/routes/ad.tsx
@@ -1,31 +1,28 @@
+import type { V2_MetaFunction } from "@remix-run/react";
 import { useSearchParams } from "@remix-run/react";
 
 import { AdLogo } from "~/components/AdLogo";
 
-const monthNames = new Set([
-	"january",
-	"february",
-	"march",
-	"april",
-	"may",
-	"june",
-	"july",
-	"august",
-	"september",
-	"october",
-	"november",
-	"december",
-]);
+export const meta: V2_MetaFunction = ({ location }) => {
+	const params = new URLSearchParams(location.search);
+	const { month, year } = getMonthAndYear(params);
+
+	if (!isValidMonth(month) || !isValidYear(year)) {
+		return [{ title: "Ad | Philly JS Club" }];
+	}
+
+	return [{ title: `Ad (${upperFirst(month)} ${year}) | Philly JS Club` }];
+};
 
 export default function Ad() {
 	const [params] = useSearchParams();
-	const month = params.get("month")?.toLowerCase() ?? "idk";
-	if (!monthNames.has(month)) {
+	const { month, year } = getMonthAndYear(params);
+
+	if (!isValidMonth(month)) {
 		return "nope (month)";
 	}
 
-	const year = params.get("year")?.toLowerCase() ?? "when";
-	if (!/\d{4}/.test(year)) {
+	if (!isValidYear(year)) {
 		return "nope (year)";
 	}
 
@@ -46,4 +43,33 @@ export default function Ad() {
 
 function upperFirst(text: string) {
 	return text[0].toUpperCase() + text.slice(1);
+}
+
+function getMonthAndYear(params: URLSearchParams) {
+	const month = params.get("month")?.toLowerCase() ?? "idk";
+	const year = params.get("year")?.toLowerCase() ?? "when";
+	return { month, year };
+}
+
+function isValidMonth(month: string) {
+	const monthNames = new Set([
+		"january",
+		"february",
+		"march",
+		"april",
+		"may",
+		"june",
+		"july",
+		"august",
+		"september",
+		"october",
+		"november",
+		"december",
+	]);
+
+	return monthNames.has(month);
+}
+
+function isValidYear(year: string) {
+	return /\d{4}/.test(year);
 }
