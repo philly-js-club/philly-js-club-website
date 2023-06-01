2ce48512c3ba2fea4ccfbb107f57b88b66a69cd1
Joe Woods <code@joewoods.dev>
2023-05-31 21:17:18 -0400

    ics-feed: add uid

--- app/routes/ics-feed[.ics].ts
+++ app/routes/ics-feed[.ics].ts
@@ -6,27 +6,29 @@ import { generateIcs } from "ics-service";
 import { site } from "~/config";
 
 export const loader: LoaderFunction = async () => {
-	const events: EventAttributes[] = (await import("../data/events.json")).map(
-		(event) => {
-			const date = new Date(event.date);
-			return {
-				// TODO: add uid: event.uid,
-				title: event.topics.join(", "),
-				location: event.location,
-				url: event.link,
-				start: [
-					date.getUTCFullYear(),
-					date.getUTCMonth() + 1,
-					date.getUTCDate(),
-					date.getUTCHours(),
-					date.getUTCMinutes(),
-				],
-				startInputType: "utc",
-				endInputType: "utc",
-				duration: event.duration as DurationObject,
-			};
-		}
-	);
+	const eventData = (await import("../data/events.json")).default;
+	eventData.reverse();
+	const events: EventAttributes[] = eventData.map((event, index) => {
+		const date = new Date(event.date);
+		return {
+			title: event.topics.join(", "),
+			location: event.location,
+			url: event.link,
+			start: [
+				date.getUTCFullYear(),
+				date.getUTCMonth() + 1,
+				date.getUTCDate(),
+				date.getUTCHours(),
+				date.getUTCMinutes(),
+			],
+			startInputType: "utc",
+			endInputType: "utc",
+			duration: event.duration as DurationObject,
+			// HACK: this relies on position in the array not changing… but
+			//       honestly, it's not a big deal if it does.
+			uid: String(index),
+		};
+	});
 	let ics = "";
 	try {
 		ics = generateIcs(site.title, events, site.baseURL);
