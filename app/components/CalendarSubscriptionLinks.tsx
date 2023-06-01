f6039312e956059d39d22408b111532bc02f6536
Joe Woods <code@joewoods.dev>
2023-05-31 20:56:21 -0400

    Style CalendarSubscriptionLinks

--- app/components/CalendarSubscriptionLinks.tsx
+++ app/components/CalendarSubscriptionLinks.tsx
@@ -10,23 +10,24 @@ export function CalendarSubscriptionLinks({
 	const [showMore, setShowMore] = useState(false);
 
 	return (
-		<div>
+		<div className="calendar-subscription">
 			<button onClick={() => setShowMore((s) => !s)}>
 				📆 Subscribe with your favorite calendar client
 			</button>
 			{showMore && (
 				<>
-					<a href={`https://www.google.com/calendar/render?cid=${webcalUrl}`}>
-						Subscribe with Google Calendar
-					</a>
-					<br />
-					To subscribe, point your calendar client to the following URL:
-					<br />
-					{webcalUrl}
-					<br />
-					⚠️ Be sure to not download and import that file! You'll need to
-					subscribe to the calendar updates in order for your calendar client to
-					continue to fetch new events.
+					<p>
+						<a href={`https://www.google.com/calendar/render?cid=${webcalUrl}`}>
+							Subscribe with Google Calendar
+						</a>
+					</p>
+					<p>To subscribe, point your calendar client to the following URL:</p>
+					<p>{webcalUrl}</p>
+					<p>
+						⚠️ Be sure to not download and import that file! You'll need to
+						subscribe to the calendar updates in order for your calendar client
+						to continue to fetch new events.
+					</p>
 				</>
 			)}
 		</div>
