804910e012d950c87a08a58ea99fdd6c086c9940
Matt Brophy <matt@brophy.org>
2023-09-15 16:12:55 -0400

    Upgrade to v2

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
 	ignoredRouteFiles: ["**/.*"],
-	future: {
-		v2_errorBoundary: true,
-		v2_meta: true,
-		v2_normalizeFormMethod: true,
-		v2_routeConvention: true,
-		v2_headers: true,
-		v2_dev: true,
-	},
+	future: {},
 	serverModuleFormat: "cjs",
 };
