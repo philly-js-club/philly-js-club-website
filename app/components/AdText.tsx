a9a94923c7eb70435fba59b7ec0acee7ad5813a7
Josh Goldberg <git@joshuakgoldberg.com>
2024-01-02 17:36:57 -0500

    Ad components

--- app/components/AdText.tsx
+++ app/components/AdText.tsx
@@ -2,6 +2,8 @@
 // https://github.com/philly-js-club/philly-js-club-website/issues/61
 "use client";
 
+import { css } from "styled-system/css";
+
 import { site } from "~/config";
 import { upperFirst } from "~/utils/common";
 import { useMonthAndYear } from "~/utils/dates";
@@ -10,9 +12,51 @@ export function AdText() {
 	const { month, year } = useMonthAndYear();
 
 	return (
-		<div className="ad-text">
-			<h1>{site.title}</h1>
-			<p>
+		<div
+			className={css({
+				base: {
+					display: "flex",
+					flexDirection: "column",
+					gap: "[2rem]",
+					textAlign: "center",
+				},
+				small: {
+					textAlign: "left",
+					width: "[50%]",
+				},
+				medium: {
+					alignContent: "flex-start",
+				},
+			})}
+		>
+			<h1
+				className={css({
+					base: {
+						fontSize: "[5rem]",
+						fontWeight: "title",
+						lineHeight: "[0.85]",
+						margin: "[0 15vw]",
+						textTransform: "uppercase",
+					},
+					small: {
+						margin: "[0]",
+					},
+					medium: {
+						fontSize: "[7rem]",
+					},
+					large: {
+						fontSize: "[10rem]",
+					},
+				})}
+			>
+				{site.title}
+			</h1>
+			<p
+				className={css({
+					fontSize: "larger",
+					fontWeight: "[700]",
+				})}
+			>
 				{upperFirst(month)} {year}
 			</p>
 		</div>
