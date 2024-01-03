a7b75ab1f99c1ee773b0085ebd253b98f25b0346
Josh Goldberg <git@joshuakgoldberg.com>
2024-01-03 17:58:24 -0500

    Fix <body>/root.tsx inclusion

--- panda.config.ts
+++ panda.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
 	exclude: [],
-	include: [
-		"./app/routes/**/*.{ts,tsx,js,jsx}",
-		"./app/components/**/*.{ts,tsx,js,jsx}",
-	],
+	include: ["./app/**/*.{j,t}s?"],
 	outdir: "styled-system",
 	outExtension: "js",
 	preflight: true,
