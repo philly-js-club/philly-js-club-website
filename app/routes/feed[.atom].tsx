5aa59f41b38b6694a4e16ad251455686dbce7f60
Tim Leonard <69484330+iamtimleonard@users.noreply.github.com>
2025-02-01 17:56:44 -0500

    #38: Add RSS support (#127)
    * feat(deps): add feed dependency to generate syndication files
    * feat(feed): create syndication route at /feed.atom
    - follow /ics-feed.ics pattern
    - adds link in head of root template
    - generates resources for each event in events.json
    * fix(feed): update props to satisfy W3C validator
    * fix (feed): Added Author as "PhillyJS Club"
    
    ---------
    
    Co-authored-by: Jason V <github@voccia.com>

---
+++ app/routes/feed[.atom].tsx
@@ -0,0 +1,43 @@
+import { Feed } from "feed";
+
+import { site } from "~/config";
+
+import eventJson from "../data/events.json";
+
+export function loader() {
+	const feed = new Feed({
+		title: site.title,
+		description: site.description,
+		id: `${site.baseURL}/`,
+		link: site.baseURL,
+		language: "en",
+		copyright: `All rights reserved ${new Date().getFullYear().toString()}, Philly JS Club`,
+		updated: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+		feedLinks: {
+			atom: `${site.baseURL}/feed.atom`,
+		},
+	});
+
+	eventJson.forEach((event) => {
+		feed.addItem({
+			title: event.topics.join(" "),
+			id: event.link,
+			link: event.link,
+			author: [{ name: "PhillyJS Club" }],
+			date: new Date(
+				new Date(event.date).getFullYear(),
+				new Date(event.date).getMonth(),
+				1,
+			),
+			content: `Date: ${new Date(event.date).toDateString()}, Location: ${event.location}, Details: ${event.link}`,
+		});
+	});
+
+	return new Response(feed.atom1(), {
+		headers: {
+			"Content-Type": "application/atom+xml",
+			"Cache-Control": "max-age=3600",
+			"Access-Control-Allow-Origin": "*",
+		},
+	});
+}
